ea4a4f8a3ec32c9d93bfd6ecd0c2c3c846ed8ece
wjdrbs96 <wjdrbs966@naver.com>
2021-04-26 22:53:22 +0900

    Test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -30,4 +30,5 @@ DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/deploy.log
 
 docker run build -d -p $IDLE_PORT:8080 gyunny
+
 #nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/deploy_err.log &
