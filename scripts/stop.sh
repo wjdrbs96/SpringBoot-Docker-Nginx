17862fc991d3910bfca5aa1ef9cb5cc79f3b2966
wjdrbs96 <wjdrbs966@naver.com>
2021-04-27 14:16:52 +0900

    Debug

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -11,6 +11,8 @@ CONTAINER_ID=$(docker container ls -f "name=${IDLE_PROFILE}" -q)
 echo "> 컨테이너 ID는 무엇?? ${CONTAINER_ID}"
 echo "> 현재 프로필은 무엇?? ${IDLE_PROFILE}"
 
+kil -9 sds
+
 if [ -z ${CONTAINER_ID} ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
