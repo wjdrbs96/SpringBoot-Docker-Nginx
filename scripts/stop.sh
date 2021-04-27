7ce339e6af9a0f3547caf50bc738b243e3831f40
wjdrbs96 <wjdrbs966@naver.com>
2021-04-27 14:24:28 +0900

    Bug Fix ~~

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -11,17 +11,13 @@ CONTAINER_ID=$(docker container ls -f "name=${IDLE_PROFILE}" -q)
 echo "> 컨테이너 ID는 무엇?? ${CONTAINER_ID}"
 echo "> 현재 프로필은 무엇?? ${IDLE_PROFILE}"
 
-if (abc) {
-  System.out.println("tt";)
-}
-
 if [ -z ${CONTAINER_ID} ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> docker stop ${IDLE_PROFILE}"
-  sudo docker stop $IDLE_PROFILE}
-  echo "> docker rm $IDLE_PROFILE}"
-  sudo docker rm $IDLE_PROFILE}
+  sudo docker stop ${IDLE_PROFILE}
+  echo "> docker rm ${IDLE_PROFILE}"
+  sudo docker rm ${IDLE_PROFILE}
   sleep 5
 fi
