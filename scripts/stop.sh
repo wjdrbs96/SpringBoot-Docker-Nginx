3455a7d11bf52cb0ec0aa6040b6c5f617ed32575
wjdrbs96 <wjdrbs966@naver.com>
2021-04-27 11:04:35 +0900

    수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,16 +4,19 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-IDLE_PORT=$(find_idle_port)
 
-echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
-IDLE_PID=$(sudo lsof -ti tcp:${IDLE_PORT})
+IDLE_PORT=$(find_idle_port)
+IDLE_PROFILE=$(find_idle_profile)
+
+#echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
+#IDLE_PID=$(sudo lsof -ti tcp:${IDLE_PORT})
 
 if [ -z ${IDLE_PID} ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $IDLE_PID"
-  kill -15 ${IDLE_PID}
+  sudo docker stop IDLE_PROFILE
+#  kill -15 ${IDLE_PID}
   sleep 5
 fi
