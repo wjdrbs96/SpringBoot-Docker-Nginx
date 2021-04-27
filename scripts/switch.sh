e57b15417e3dbd106f0b736ec7f8e0f1c7ad27d5
wjdrbs96 <wjdrbs966@naver.com>
2021-04-27 15:16:26 +0900

    Debug~

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -11,8 +11,9 @@ function switch_proxy() {
     echo "> Port 전환"
     echo "set \$service_url http://3.36.209.141:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
-
     echo "> 엔진엑스 Reload ${IDLE_PORT}"
-    sudo docker exec -it nginx nginx -s reload
+    sudo docker exec -itu nginx nginx -s reload
     echo "> docker exec -it nginx nginx -s reload"
 }
+
+
