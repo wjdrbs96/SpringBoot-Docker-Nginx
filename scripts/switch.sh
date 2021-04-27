014cfb5cdbe14c8cc8331043a4ac3de7cbc3de26
wjdrbs96 <wjdrbs966@naver.com>
2021-04-27 11:16:51 +0900

    Test

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -3,6 +3,7 @@
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
+REPOSITORY=/home/ec2-user/app/step4
 
 function switch_proxy() {
     IDLE_PORT=$(find_idle_port)
@@ -11,6 +12,7 @@ function switch_proxy() {
     echo "> Port 전환"
     echo "set \$service_url http://3.36.209.141:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
+    cd REPOSITORY
     echo "> 엔진엑스 Reload"
     sudo docker exec -it nginx nginx -s reload
     #sudo service nginx reload
